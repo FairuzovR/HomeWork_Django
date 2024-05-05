865065f4d3591721bbb5d482ca176d09251fdff2
FAIRU <skyfairrr@gmail.com>
2024-05-06 01:29:25 +0500

    finished 20.2

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product
 
 # Create your views here.
 
@@ -7,3 +8,13 @@ def home(request):
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products" : products}
+    return render(request, 'catalog/products_list.html', context)
+
+def product_date(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'catalog/product_date.html', context)
