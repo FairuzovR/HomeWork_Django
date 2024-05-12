790abe396b4ddc986ae71c9baa43b8321307e29c
FAIRU <skyfairrr@gmail.com>
2024-05-12 17:17:03 +0500

    finished 21.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from catalog.views import home, contacts, products_list, product_date
+from catalog.views import home, contacts, ProductsListView, ProductView
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('products_list/', products_list, name='products_list'),
-    path('products_list/<int:pk>/', product_date, name='product_date')
+    path('products_list/', ProductsListView.as_view(), name='products_list'),
+    path('products_list/<int:pk>/', ProductView.as_view(), name='product_detail')
 ]
