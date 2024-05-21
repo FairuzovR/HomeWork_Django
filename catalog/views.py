b0283b952566f22f689c9b07d112e17699695d06
FAIRU <skyfairrr@gmail.com>
2024-05-21 23:03:16 +0500

    finished 22.1 (fix gitignore and views)

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from catalog.models import Product, Version
 from django.views.generic import (
     CreateView, ListView, DetailView, DetailView, UpdateView, DeleteView)
 from catalog.forms import ProductForm
 from django.urls import reverse_lazy, reverse
 from pytils.translit import slugify
-from django.shortcuts import get_object_or_404, redirect
 
 # Create your views here.
 
@@ -19,7 +18,15 @@ def contacts(request):
 class ProductsCreateView(CreateView):
     model = Product
     form_class = ProductForm
-    success_url = reverse_lazy('catalog:list')
+    success_url = reverse_lazy('catalog:products_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_product = form.save()
+            new_product.slug = slugify(new_product.name)
+            new_product.save()
+
+        return super().form_valid(form)
 
 class ProductsUpdateView(UpdateView):
     model = Product
@@ -42,12 +49,9 @@ class ProductsListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context["version"] = Version.objects.filter(current_version=True).first()
         for object in context.get('object_list'):
             version = Version.objects.filter(current_version=True, product_id=object.pk).first()
             object.version = version
-            print(object.version)
-            # product.version = product.version_set.filter(is_active=True).first()
         return context
 
 
